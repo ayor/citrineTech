c1f3ab52477dfaaccde1a19372d915cbdd3799b9
ayor <dosumuayomide@gmail.com>
2021-08-14 10:09:08 +0100

    logging prod url

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -57,6 +57,7 @@ const Contact = () => {
             }
             //
             // let response = await axios.post(`${server}/api/message`,
+            console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
             let response = await axios.post(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/message`,
                 {
                     ...data
