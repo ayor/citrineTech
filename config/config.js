fefd6a85909e51be8444fb82859643c2b6fe020c
ayor <dosumuayomide@gmail.com>
2021-08-14 08:33:34 +0100

    prod-test-2

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== "production" ? "dev" : null;
 
-export const server = dev ? "http://localhost:3000" : "https://citrine-tech-i9u1h3gvf-ayor.vercel.app";
+export const server = dev ? "http://localhost:3000" : process.env.VERCEL_URL;
 
 
 
