a5c915203a7005cf9bc5dd45d415a9a115c4fc0d
ayor <dosumuayomide@gmail.com>
2021-08-14 15:20:07 +0100

    fixed the classname of the spinner

--- components/Spinner/spinner.js
+++ components/Spinner/spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Spinner = () => (<span class="spinner-border" role="status">
-    <span class="sr-only">Loading...</span>
+const Spinner = () => (<span className="spinner-border" role="status">
+    <span className="sr-only">Loading...</span>
 </span>)
 
 export default Spinner;
