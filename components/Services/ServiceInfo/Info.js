6d3e4e39ea74a52a437a97f57841399e21b9ebe0
ayor <dosumuayomide@gmail.com>
2021-08-23 07:42:58 +0100

    made products and services reactive

---
+++ components/Services/ServiceInfo/Info.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Styles from '../../styles/Products.module.css'
+
+const Info = (props) => {
+    <div className="row border-top my-2 ">
+
+    </div>
+}
+
+export default Info; 
