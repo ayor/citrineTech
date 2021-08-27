b2f095dc6fee851b76d9cd922ddbd73f4fb56d3f
ayor <dosumuayomide@gmail.com>
2021-08-27 16:26:34 +0100

    removed products page

--- components/Services/ServiceInfo/Info.js
+++ components/Services/ServiceInfo/Info.js
@@ -2,9 +2,9 @@ import React from "react";
 import styles from '../../../styles/Products.module.css'
 
 const Info = (props) => (
-<div className={styles.ServiceInfo}>
-{props.children}
-</div>
+    <div className={styles.ServiceInfo}>
+        {props.children}
+    </div>
 );
 
 export default Info;
