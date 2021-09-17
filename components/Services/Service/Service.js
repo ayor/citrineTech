e2f9a33a017623355fb79a3114f15c3182379647
ayor <dosumuayomide@gmail.com>
2021-09-17 07:48:29 +0100

    cintrine tech redesign

--- components/Services/Service/Service.js
+++ components/Services/Service/Service.js
@@ -1,39 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Styles from '../../../styles/Products.module.css'
-
+import Image from 'next/image'; 
 
 
 const Service = (props) => {
     const [classes, setClasses] = useState(["p-3", Styles.ServiceCard]);
 
 
-    const handleScroll = (event) => {
-        if (window.scrollY >= 200) {
+    // const handleScroll = (event) => {
+    //     if (window.scrollY >= 200) {
 
-            let arr = [...classes, props.class];
-            setClasses(arr);
+    //         let arr = [...classes, props.class];
+    //         setClasses(arr);
 
-        } else {
-            // setIsSet(false);
-            setClasses(["p-3", Styles.ServiceCard])
-        }
-    }
+    //     } else {
+    //         // setIsSet(false);
+    //         setClasses(["p-3", Styles.ServiceCard])
+    //     }
+    // }
 
-    useEffect(() => {
-        document.addEventListener("scroll", handleScroll);
-        return () => {
-            document.removeEventListener("scroll", handleScroll)
-        }
-    }, [handleScroll])
+    // useEffect(() => {
+    //     document.addEventListener("scroll", handleScroll);
+    //     return () => {
+    //         document.removeEventListener("scroll", handleScroll)
+    //     }
+    // }, [handleScroll])
 
-    return (<div className={classes.join(' ')}>
-        <i className={`fa fa-${props.iconName} fa-4x text-secondary mb-2`}></i>
+    return (
+    // <div className={classes.join(' ')}>
+     <div className="text-center">
+        {/* <i className={`fa fa-${props.iconName} fa-4x text-secondary mb-2`}></i>
         <h3 className="text-muted mb-3">{props.header}</h3>
         <p className="text-muted">{props.text}
-        </p>
-        {/* <div>
-            
-        </div> */}
+        </p> */}
     </div>
     )
 };
