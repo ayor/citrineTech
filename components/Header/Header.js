87fa2fc401ee2e9b15fb81a4a77dc8110ca4bf0d
ayor <dosumuayomide@gmail.com>
2021-10-09 16:42:16 +0100

    fix & updates for 9oct2021

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,76 +1,108 @@
-import React, { useEffect, useState, useRef } from 'react'
-import Btn from '../Button/Button';;
+import React, { useEffect, useState, useRef } from 'react';
+import Btn from '../Button/Button';
 import HeaderStyles from '../../styles/Header.module.css';
 import UnderLine from './Underline';
 import Image from 'next/image';
-import citrineSrc from '../../public/img/CITRINEFRONTPAGE.png'; 
+import citrineSrc from '../../public/img/CITRINEFRONTPAGE.png';
 
 const Header = ({ title, bannerMessage, showbtn }) => {
-    // const [vantaEffect, setVantaEffect] = useState(0)
-    const myRef = useRef(null)
+  // const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null);
 
-    // useEffect(() => {
-    //     if (!vantaEffect) {
-    //         setVantaEffect(
-    //             NET({
-    //                 el: myRef.current,
-    //                 THREE,
-    //                 color: 0x311e1e,
-    //                 // backgroundColor: 0x848444,
-    //                 backgroundColor: 0xb3b343,
-    //                  points: 15.00,
-    //                 maxDistance: 30.00
-    //             })
-    //             // RING({
-    //             //     el: myRef.current,
-    //             //     THREE,
-    //             //     backgroundColor: 0xc3c24f
-    //             // })
-    //         )
-    //     }
-    //     return () => {
-    //         if (vantaEffect) {
-    //             vantaEffect.destroy()
-    //         }
-    //     }
-    // }, [vantaEffect])
+  // useEffect(() => {
+  //     if (!vantaEffect) {
+  //         setVantaEffect(
+  //             NET({
+  //                 el: myRef.current,
+  //                 THREE,
+  //                 color: 0x311e1e,
+  //                 // backgroundColor: 0x848444,
+  //                 backgroundColor: 0xb3b343,
+  //                  points: 15.00,
+  //                 maxDistance: 30.00
+  //             })
+  //             // RING({
+  //             //     el: myRef.current,
+  //             //     THREE,
+  //             //     backgroundColor: 0xc3c24f
+  //             // })
+  //         )
+  //     }
+  //     return () => {
+  //         if (vantaEffect) {
+  //             vantaEffect.destroy()
+  //         }
+  //     }
+  // }, [vantaEffect])
 
-    const headerTitle = !title ? "Better, Faster & Efficient IT Services for you!" : title;
-    const headerMessage = !bannerMessage ? `Protection, Preparedness & Productivity ` : bannerMessage;
-    if (showbtn != false) {
-        showbtn = true;
-    }
+  const headerTitle = !title
+    ? 'Better, Faster & Efficient IT Services for you!'
+    : title;
+  const headerMessage = !bannerMessage
+    ? `Protection, Preparedness & Productivity `
+    : bannerMessage;
+  if (showbtn != false) {
+    showbtn = true;
+  }
 
-
-    return (
-        <section className={"container px-3 " + HeaderStyles.header}>
-           
-            <header ref={myRef} className={`row`}>
+  return (
+    <section className={'container px-3 ' + HeaderStyles.header}>
+      <header ref={myRef} className={`row`}>
+        <div className="col">
+          <div className="row ">
+            <div className="col ">
+              <div className="row">
                 <div className="col">
-                    <div className="row ">
-                        <div className="col ">
-                            <div className={"text-dark p-2 " + HeaderStyles.banner}>
-                                <small className="text-secondary mt-2"> {headerMessage}</small>
-                                <h1 className={"font-weight-bold text-dark pl-4 mb-4 " + HeaderStyles.bannerHeading}>Your technology partner with a focus on providing proactive solutions</h1>
-                                {/* <UnderLine /> */}
-                                {showbtn ? <Btn btnHref={"#contact"} title={"Contact Us"} /> : null}
-                            </div>
-                            <div className={HeaderStyles.HeaderImg}>
-                               {/* <Image src={citrineSrc}  layout="fill"/> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row mt-3">
-                        <div className={"card col-md-4 offset-md-7 pt-4 px-4  " + HeaderStyles.WelcomeMessage}>
-                            <p className="text-secondary card-title">Citrine is an IT company that offers timely services as well as a vision for technology that will grow with your business. We provide businesses with affordable and reliable IT services aimed at addressing Protection, Preparedness, and Productivity.</p>
-                        </div>
-                    </div>
+                  <div
+                    className={'text-dark p-2 ' + HeaderStyles.banner}
+                    style={{ zIndex: 999 }}
+                  >
+                    <small className="text-secondary mt-2">
+                      {' '}
+                      {headerMessage}
+                    </small>
+                    <h1
+                      className={
+                        'font-weight-bold text-dark pl-4 mb-4 ' +
+                        HeaderStyles.bannerHeading
+                      }
+                    >
+                      Your technology partner with a focus on providing
+                      proactive solutions
+                    </h1>
+                    {/* <UnderLine /> */}
+                    {showbtn ? (
+                      <Btn btnHref={'#contact'} title={'Contact Us'} />
+                    ) : null}
+                  </div>
                 </div>
-            </header>
-        </section >
-    )
+                <div className="col">
+                  <div className={HeaderStyles.HeaderImg}>
+                    {/* <Image src={citrineSrc}  layout="fill"/> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div
+              className={
+                'card col-md-4 offset-md-7 pt-4 px-4  ' +
+                HeaderStyles.WelcomeMessage
+              }
+            >
+              <p className="text-secondary card-title">
+                Citrine is an IT company that offers timely services as well as
+                a vision for technology that will grow with your business. We
+                provide businesses with affordable and reliable IT services
+                aimed at addressing Protection, Preparedness, and Productivity.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default Header;
-
-
--- components/Services/data.js
+++ components/Services/data.js
@@ -1,110 +1,117 @@
 export const data = [
-    {
-        id: 3,
-        header: "Managed IT Services",
-        hasSubheading: true,
-        subheading: [
-            {
-                header: "Features ",
-                detail: {
-                    hasText: false,
-                    // text: "With all of the cloud-based applications and big data stressing your network, how proactive are you today in ensuring maximum network performance and uptime?",
-                    list: [
-                        "Access to a large team of highly trained and experienced Engineers (NOC)",
-                        "Improved service with a guaranteed service level agreement",
-                        "Allow you to properly budget for your IT services",
-                        "Access to highly resilience and highly secured IT infrastructure",
-                        "Proactive Support",
-                        "Improved IT security",
-                        "Management and Status Reporting",
-                        "Best Practice Driven",
-                        "Automated and Reliable",
-                        "Better focus on business goals and projects"
-                    ]
-                }
-            }
-        ],
-        hasSecondParagraph: true,
-        mainText: `The Citrine Technologies Managed IT Services program provides affordable proactive IT management, monitoring, and support to growing businesses at a fixed monthly fee. Utilizing our unique framework for providing managed IT services, Citrine Technologies provides a range of proactive services to keep your computer systems, IT infrastructure up and running and your people and business productive.`,
-        secondParagraph: `Our goal is to serve as your technology partner with a focus on providing proactive solutions at a predictable monthly fee. We use a consultative approach to evaluate your business and technology needs and then advise on the best solutions for your current and future needs. Citrine Technologies can save your organization time
+  {
+    id: 3,
+    header: 'Managed IT Services',
+    hasSubheading: true,
+    subheading: [
+      {
+        header: 'Features ',
+        detail: {
+          hasText: false,
+          // text: "With all of the cloud-based applications and big data stressing your network, how proactive are you today in ensuring maximum network performance and uptime?",
+          list: [
+            'Access to a large team of highly trained and experienced Engineers (NOC)',
+            'Improved service with a guaranteed service level agreement',
+            'Allow you to properly budget for your IT services',
+            'Access to highly resilience and highly secured IT infrastructure',
+            'Proactive Support',
+            'Improved IT security',
+            'Management and Status Reporting',
+            'Best Practice Driven',
+            'Automated and Reliable',
+            'Better focus on business goals and projects',
+          ],
+        },
+      },
+    ],
+    hasSecondParagraph: true,
+    mainText: `The Citrine Technologies Managed IT Services program provides affordable proactive IT management, monitoring, and support to growing businesses at a fixed monthly fee. Utilizing our unique framework for providing managed IT services, Citrine Technologies provides a range of proactive services to keep your computer systems, IT infrastructure up and running and your people and business productive.`,
+    secondParagraph: `Our goal is to serve as your technology partner with a focus on providing proactive solutions at a predictable monthly fee. We use a consultative approach to evaluate your business and technology needs and then advise on the best solutions for your current and future needs. Citrine Technologies can save your organization time
         and money through better use of appropriate technologies.`,
 
-        imageSrc: '../../img/support.jpg',
-    },
-    {
-        id: 1,
-        header: "Enterprise Automation",
-        hasSubheading: true,
-        subheading: [{
-            header: "BENEFITS OF PROCESS AUTOMATION",
-            detail: {
-                hasText: false,
-                list: [
-                    "Increase focus on more strategic initiatives",
-                    "Grow your organization",
-                    "Boost productivity",
-                    "Improve transparency",
-                    "Reduce costs",
-                    "Spark innovation",
-                    "Identify bottlenecks early",
-                    "Make audits a breeze",
-                ]
-            }
-        }],
-        hasSecondParagraph: true,
-        mainText: `Business leaders are challenging their organizations’ to achieve transformation by harnessing digital technologies with organizational, operational, and business innovation to create new ways of operating and growing businesses. Optimizing IT infrastructure and operational processes through automation is key to successfully achieving digital transformation.`,
-        secondParagraph: `At Citrine, we use technology to automate routine tasks, such as data entry, routing information between parties,
+    imageSrc: '../../img/support.jpg',
+  },
+  {
+    id: 1,
+    header: 'Enterprise Automation',
+    hasSubheading: true,
+    subheading: [
+      {
+        header: 'BENEFITS OF PROCESS AUTOMATION',
+        detail: {
+          hasText: false,
+          list: [
+            'Increase focus on more strategic initiatives',
+            'Grow your organization',
+            'Boost productivity',
+            'Improve transparency',
+            'Reduce costs',
+            'Spark innovation',
+            'Identify bottlenecks early',
+            'Make audits a breeze',
+          ],
+        },
+      },
+    ],
+    hasSecondParagraph: true,
+    mainText: `Business leaders are challenging their organizations’ to achieve transformation by harnessing digital technologies with organizational, operational, and business innovation to create new ways of operating and growing businesses. Optimizing IT infrastructure and operational processes through automation is key to successfully achieving digital transformation.`,
+    secondParagraph: `At Citrine, we use technology to automate routine tasks, such as data entry, routing information between parties,
         Organizing documents and more, aimed at making human lives easier by transforming time-consuming busywork into seamless processes that accelerate how work gets done whilst improving productivity and efficiency.`,
-        imageSrc: '../../img/automation.jpg',
-    },
-    {
-        id: 2,
-        header: "Telephony and Communication",
-        mainText: `We offer a complete cost-effective telephone solution platform with an open source systems & appliances addressing a wide range of call centre/contact centre solutions & enterprises solutions including IVR systems, open source VoIP PBX, call centre dialler, voice dialler, voice logger, automatic call distribution, predictive dialler, hosted dialler, GSM & SMS integration, VoIP call recording, customized line call recording / monitoring / tracking.
-
-        Our wide range of products is the perfect companion for frequent callers demanding longer talk time.
-        We also provide support services, routed through our helpdesk suite and bounded by SLAs. Our products are both user and budget friendly giving you value for your investment.
+    imageSrc: '../../img/automation.jpg',
+  },
+  {
+    id: 2,
+    header: 'Telephony and Communication',
+    hasSecondParagraph: true,
+    mainText: `We offer a complete cost-effective telephone solution platform with an open source systems & appliances addressing a wide range of call centre/contact centre solutions & enterprises solutions including IVR systems, open source VoIP PBX, call centre dialler, voice dialler, voice logger, automatic call distribution, predictive dialler, hosted dialler, GSM & SMS integration, VoIP call recording, customized line call recording / monitoring / tracking.
+         `,
+    secondParagraph: `Our wide range of products is the perfect companion for frequent callers demanding longer talk time.
+         We also provide support services, routed through our helpdesk suite and bounded by SLAs. Our products are both user and budget friendly giving you value for your investment.
         `,
-        hasSubheading: true,
-        subheading: [{
-            header: "Our solutions help:",
-            detail: {
-                hasText: false,
-                list: [
-                    "Solving the communication issues for enterprises",
-                    "Deliver cost benefits by automating customer interaction",
-                    "Connect your employees remotely efficiently",
-                    "Manage call quality, and much more."
-                ]
-            }
-        }],
-        imageSrc: '../../img/services.png',
-    },
-    {
-        id: 4,
-        header: "Infrastructure Security Services",
-        mainText: ` New threats are emerging daily and it can sometimes be difficult to know if you are at risk, from and how severe the impact of these threats could be. To manage and control your security risks, it is important to identify system and network vulnerabilities that may be exploited by intruders, and to take countermeasure to prevent any possible attack.
+    hasSubheading: true,
+    subheading: [
+      {
+        header: 'Our solutions help:',
+        detail: {
+          hasText: false,
+          list: [
+            'Solving the communication issues for enterprises',
+            'Deliver cost benefits by automating customer interaction',
+            'Connect your employees remotely efficiently',
+            'Manage call quality, and much more.',
+          ],
+        },
+      },
+    ],
+    imageSrc: '../../img/service.jpeg',
+  },
+  {
+    id: 4,
+    header: 'Infrastructure Security Services',
+    mainText: ` New threats are emerging daily and it can sometimes be difficult to know if you are at risk, from and how severe the impact of these threats could be. To manage and control your security risks, it is important to identify system and network vulnerabilities that may be exploited by intruders, and to take countermeasure to prevent any possible attack.
 
          `,
-        hasSecondParagraph: true,
-        secondParagraph: `
+    hasSecondParagraph: true,
+    secondParagraph: `
         Citrine Technologies Ltd shall develop a methodology of helping you to understand your specific cyber domain, and how you can reduce the threats you face and become both cyber aware and cyber secure. Our Infrastructure Security service will provide your organization with a detailed report on your vulnerabilities and a range of recommendations to help you overcome these issues. 
                 
                 This service will not only enable you to become cyber aware but also give you a detailed understanding of cybersecurity and the elements included within it.`,
-        imageSrc: '../../img/infrastructure.jpg',
-        hasSubheading: true,
-        subheading: [{
-            header: "Our Infrastructure Audit service covers:",
-            detail: {
-                hasText: false,
-                list: [
-                    "Vulnerability Assessment",
-                    "Information Process Audit",
-                    "Penetration Test",
-                    "Gap Remediation"
-                ]
-            }
-        }],
-    },
-]
-
+    imageSrc: '../../img/infrastructure.jpg',
+    hasSubheading: true,
+    subheading: [
+      {
+        header: 'Our Infrastructure Security Services Provide:',
+        detail: {
+          hasText: false,
+          list: [
+            'Manged IT Services',
+            'Vulnerability Assessment',
+            'Information Process Audit',
+            'Penetration Test',
+            'Gap Remediation',
+          ],
+        },
+      },
+    ],
+  },
+];
