da3984ea37e86e4275e8bfc5cac8523caf9e4108
ayor <dosumuayomide@gmail.com>
2021-10-14 07:56:26 +0100

    fix eror

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -45,43 +45,47 @@ const Header = ({ title, bannerMessage, showbtn }) => {
     showbtn = true;
   }
 
-  return (<>
-    <section className={'container px-3 ' + HeaderStyles.header}>
-      <header ref={myRef} className={`row`}>
-        <div className="col">
-          <div
-            // + HeaderStyles.banner
-            className={'text-dark p-2 ' + HeaderStyles.HeaderImg}
-          >
-            <small className="text-secondary mt-2"> {headerMessage}</small>
-            <h1
-              className={
-                'font-weight-bold text-dark pl-4 mb-4 ' +
-                HeaderStyles.bannerHeading
-              }
+  return (
+    <>
+      <section className={'container px-3 ' + HeaderStyles.header}>
+        <header ref={myRef} className={`row`}>
+          <div className="col">
+            <div
+              // + HeaderStyles.banner
+              className={'text-dark p-2 ' + HeaderStyles.HeaderImg}
             >
-              Your technology partner with a focus on providing proactive
-              solutions
-            </h1>
-            {/* <UnderLine /> */}
-            {showbtn ? <Btn btnHref={'#contact'} title={'Contact Us'} /> : null}
+              <small className="text-secondary mt-2"> {headerMessage}</small>
+              <h1
+                className={
+                  'font-weight-bold text-dark pl-4 mb-4 ' +
+                  HeaderStyles.bannerHeading
+                }
+              >
+                Your technology partner with a focus on providing proactive
+                solutions
+              </h1>
+              {/* <UnderLine /> */}
+              {showbtn ? (
+                <Btn btnHref={'#contact'} title={'Contact Us'} />
+              ) : null}
+            </div>
+          </div>
+        </header>
+      </section>
+      <div className="row mt-3">
+        <div className="col-md-4 col-sm-8  offset-sm-2 offset-md-7 ">
+          <div className={'card pt-4 px-4 ' + HeaderStyles.WelcomeMessage}>
+            <p className="text-secondary card-title">
+              Citrine is an IT company that offers timely services as well as a
+              vision for technology that will grow with your business. We
+              provide businesses with affordable and reliable IT services aimed
+              at addressing Protection, Preparedness, and Productivity.
+            </p>
           </div>
         </div>
-      </header>
-    </section>
-    <div className="row mt-3">
-      <div className="col-md-4 col-sm-8  offset-sm-2 offset-md-7 ">
-        <div className={'card pt-4 px-4 ' + HeaderStyles.WelcomeMessage}>
-          <p className="text-secondary card-title">
-            Citrine is an IT company that offers timely services as well as a
-            vision for technology that will grow with your business. We provide
-            businesses with affordable and reliable IT services aimed at
-            addressing Protection, Preparedness, and Productivity.
-          </p>
-        </div>
       </div>
-    </div>
-  </>
+    </>
   );
+};
 
 export default Header;
